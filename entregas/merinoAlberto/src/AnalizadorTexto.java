1eb339cf7a8c5ca7c0751b058ed23e96ba27d894
albertomeriino <alberto.merino@alumnos.uneatlantico.es>
2026-01-09 16:13:33 +0100

    Ajuste final y dar formato.

--- entregas/merinoAlberto/src/AnalizadorTexto.java
+++ entregas/merinoAlberto/src/AnalizadorTexto.java
@@ -8,90 +8,104 @@ class AnalizadorTexto {
         System.out.println("Introduce un texto (mínimo 10 caracteres):");
         String texto = sc.nextLine();
 
+        boolean textoValido = true;
         if (texto.length() < 10) {
             System.out.println("El texto es demasiado corto");
-        } else {
-
-        int vocales, consonantes, numeros, espacios, otros;
-        int[] contadores = new int[5];
-
-        for (int i = 0; i < texto.length(); i++) {
-            char c = texto.charAt(i);
-
-            if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
-
-             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
-                || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
-            vocales++;
-        } else {
-            consonantes++;
+            textoValido = false;
         }
 
-        } else if (c >= '0' && c <= '9') {
-            numeros++;
-        } else if (c == ' ') {
-            espacios++;
-        } else {
-            otros++;
-        }
-        }
+        if (textoValido) {
+            int longitudTexto = texto.length();
+            int vocales = 0;
+            int consonantes = 0;
+            int numeros = 0;
+            int espacios = 0;
+            int otros = 0;
 
-        System.out.println("ESTADÍSTICAS BÁSICAS");
-        System.out.println("Longitud: " + texto.length());
-        System.out.println("Vocales: " + vocales);
-        System.out.println("Consonantes: " + consonantes);
-        System.out.println("Números: " + numeros);
-        System.out.println("Espacios: " + espacios);
-        System.out.println("Otros: " + otros);
+            for (int i = 0; i < longitudTexto; i++) {
+                char c = texto.charAt(i);
 
-        if (numeros > 5 && otros >= 2 && espacios < longitudTexto / 10) {
-        System.out.println("Categoría: Código o expresión matemática");
+                boolean esLetra = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+                boolean esNumero = (c >= '0' && c <= '9');
 
-        } else if (vocales > consonantes && espacios >= 2 && numeros == 0) {
-        System.out.println("Categoría: Texto literario con alta densidad vocálica");
-
-        } else if (consonantes > vocales && (otros >= 3 || numeros >= 2)) {
-        System.out.println("Categoría: Texto técnico o científico");
-
-        } else if (espacios > longitudTexto / 5) {
-        System.out.println("Categoría: Texto informal o conversacional");
-
-        } else {
-        System.out.println("Categoría: Texto genérico");
-        }
-
-
-        double densidadVocales = (vocales * 100.0) / texto.length();
-        if (densidadVocales > 45 && vocales > 10 && consonantes > 5 && espacios > 2) {
-            System.out.println("¡Texto muy fluido!");
-        } else if (densidadVocales < 25 && consonantes > 10 && (otros > 5 || numeros > 3)) {
-            System.out.println("Texto complejo de leer");
-        }
-
-        boolean tieneMayuscula = false;
-        boolean tieneMinuscula = false;
-
-        for (int i = 0; i < texto.length(); i++) {
-            char c = texto.charAt(i);
-
-            if (c >= 'A' && c <= 'Z') {
-                tieneMayuscula = true;
-            } else if (c >= 'a' && c <= 'z') {
-                tieneMinuscula = true;
+                if (esLetra) {
+                    boolean esVocal = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                                   || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+                    if (esVocal) {
+                        vocales++;
+                    } else {
+                        consonantes++;
+                    }
+                } else if (esNumero) {
+                    numeros++;
+                } else if (c == ' ') {
+                    espacios++;
+                } else {
+                    otros++;
+                }
             }
-            if (tieneMayuscula && tieneMinuscula) {
-            break;
+
+            System.out.println("ESTADÍSTICAS BÁSICAS");
+            System.out.println("Longitud: " + longitudTexto);
+            System.out.println("Vocales: " + vocales);
+            System.out.println("Consonantes: " + consonantes);
+            System.out.println("Números: " + numeros);
+            System.out.println("Espacios: " + espacios);
+            System.out.println("Otros: " + otros);
+
+            if (numeros > 5 && otros >= 2 && espacios < longitudTexto / 10) {
+                System.out.println("Categoría: Código o expresión matemática");
+            } else if (vocales > consonantes && espacios >= 2 && numeros == 0) {
+                System.out.println("Categoría: Texto literario con alta densidad vocálica");
+            } else if (consonantes > vocales && (otros >= 3 || numeros >= 2)) {
+                System.out.println("Categoría: Texto técnico o científico");
+            } else if (espacios > longitudTexto / 5) {
+                System.out.println("Categoría: Texto informal o conversacional");
+            } else {
+                System.out.println("Categoría: Texto genérico");
             }
-        }
 
+            int palabras = 0;
+            for (int i = 0; i < longitudTexto; i++) {
+                if (texto.charAt(i) != ' ' && (i == 0 || texto.charAt(i - 1) == ' ')) {
+                    palabras++;
+                }
+            }
+            System.out.println("Palabras estimadas: " + palabras);
 
-        if (tieneMayuscula && tieneMinuscula && texto.charAt(0) >= 'A' && texto.charAt(0) <= 'Z' && (texto.charAt(texto.length() - 1) == '.' || texto.charAt(texto.length() - 1) == '!' || texto.charAt(texto.length() - 1) == '?') && espacios > 0) {
-            System.out.println("Formato: Oración bien formada");
-        } else if (tieneMayuscula && !tieneMinuscula && numeros >= 2 && espacios < texto.length() / 10) {
-            System.out.println("Formato: Código o identificador");
-        } else {
-            System.out.println("Formato: Variado");
-        }
+            double densidadVocales = (vocales * 100.0) / longitudTexto;
+            if (densidadVocales > 45 && vocales > 10 && consonantes > 5 && espacios > 2) {
+                System.out.println("¡Texto muy fluido!");
+            } else if (densidadVocales < 25 && consonantes > 10 && (otros > 5 || numeros > 3)) {
+                System.out.println("Texto complejo de leer");
+            }
+
+            boolean tieneMayuscula = false;
+            boolean tieneMinuscula = false;
+            for (int i = 0; i < longitudTexto; i++) {
+                char c = texto.charAt(i);
+                if (c >= 'A' && c <= 'Z') {
+                    tieneMayuscula = true;
+                } else if (c >= 'a' && c <= 'z') {
+                    tieneMinuscula = true;
+                }
+                if (tieneMayuscula && tieneMinuscula) {
+                    break;
+                }
+            }
+
+            char primera = texto.charAt(0);
+            char ultima = texto.charAt(longitudTexto - 1);
+            boolean empiezaMayuscula = primera >= 'A' && primera <= 'Z';
+            boolean terminaBien = ultima == '.' || ultima == '!' || ultima == '?';
+
+            if (tieneMayuscula && tieneMinuscula && empiezaMayuscula && terminaBien) {
+                System.out.println("Formato: Oración bien formada");
+            } else if (tieneMayuscula && !tieneMinuscula && numeros >= 2) {
+                System.out.println("Formato: Código o identificador");
+            } else {
+                System.out.println("Formato: Variado");
+            }
         }
     }
 }
