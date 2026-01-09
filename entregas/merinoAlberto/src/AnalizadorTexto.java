9da62094cb871ee852566ad624ed58367d44a9c2
albertomeriino <alberto.merino@alumnos.uneatlantico.es>
2026-01-09 15:48:25 +0100

    Reajuste de lo anterior, en vez de poner 5 variables sueltas, que es visualmente peor, se pone un unico array que es igual de efectivo.

--- entregas/merinoAlberto/src/AnalizadorTexto.java
+++ entregas/merinoAlberto/src/AnalizadorTexto.java
@@ -12,11 +12,8 @@ class AnalizadorTexto {
             System.out.println("El texto es demasiado corto");
         } else {
 
-        int vocales = 0;
-        int consonantes = 0;
-        int numeros = 0;
-        int espacios = 0;
-        int otros = 0;
+        int vocales, consonantes, numeros, espacios, otros;
+        int[] contadores = new int[5];
 
         for (int i = 0; i < texto.length(); i++) {
             char c = texto.charAt(i);
@@ -37,7 +34,7 @@ class AnalizadorTexto {
         } else {
             otros++;
         }
-    }
+        }
 
         System.out.println("ESTADÍSTICAS BÁSICAS");
         System.out.println("Longitud: " + texto.length());
@@ -48,24 +45,21 @@ class AnalizadorTexto {
         System.out.println("Otros: " + otros);
 
         if (vocales > consonantes && consonantes > 0 && texto.length() > 20 && espacios >= 2 && numeros == 0) {
-            System.out.println("Categoría: Texto literario con alta densidad vocálica");
+        System.out.println("Categoría: Texto literario con alta densidad vocálica");
+
         } else if (consonantes > vocales && vocales > 0 && texto.length() > 15 && (otros >= 3 || numeros >= 2) && espacios >= 1) {
-            System.out.println("Categoría: Texto técnico o científico");
+        System.out.println("Categoría: Texto técnico o científico");
+
         } else if (numeros > 5 && (vocales + consonantes) < texto.length() / 2 && espacios < texto.length() / 10 && otros >= 2) {
-            System.out.println("Categoría: Código o expresión matemática");
+        System.out.println("Categoría: Código o expresión matemática");
+
         } else if (espacios > texto.length() / 5 && vocales > 0 && consonantes > 0 && numeros < 3 && otros < vocales / 2) {
-            System.out.println("Categoría: Texto informal o conversacional");
+        System.out.println("Categoría: Texto informal o conversacional");
+
         } else {
-            System.out.println("Categoría: Texto genérico");
+        System.out.println("Categoría: Texto genérico");
         }
 
-        int palabras = 1;
-        for (int i = 0; i < texto.length(); i++) {
-            if (texto.charAt(i) == ' ' && i < texto.length() - 1 && texto.charAt(i + 1) != ' ') {
-                palabras++;
-            }
-        }
-        System.out.println("Palabras estimadas: " + palabras);
 
         double densidadVocales = (vocales * 100.0) / texto.length();
         if (densidadVocales > 45 && vocales > 10 && consonantes > 5 && espacios > 2) {
@@ -93,6 +87,6 @@ class AnalizadorTexto {
         } else {
             System.out.println("Formato: Variado");
         }
+        }
     }
-}
 }
